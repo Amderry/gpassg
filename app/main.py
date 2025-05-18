81a8713b2bacca639337bc28efc6ea9f27d4c4c5
Фёдор Ч. <alez1934@yandex.ru>
2025-05-18 21:41:14 +0300

    добавить: мидлварь и путь для челленджа

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, Depends
 from routers import publickey, entry
+from routers import challenge as challenge_router
+from middleware import challenge
+from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
 app.include_router(publickey.router, prefix="/publickey")
 app.include_router(entry.router, prefix="/entry")
+app.include_router(challenge_router.router, prefix="/challenge")
+app.add_middleware(BaseHTTPMiddleware, dispatch=challenge.challenge_middleware)
