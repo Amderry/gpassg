0c03009a25ec324453e763b426c290c050262686
Фёдор Ч. <alez1934@yandex.ru>
2025-05-18 10:19:30 +0300

    изменить: использовать в redis вместо get и set hget и hset

--- app/database/database_redis.py
+++ app/database/database_redis.py
@@ -5,8 +5,8 @@ class Redis(Database):
   def __init__(self, host: str, port: int, password: str = None, username: str = None):
     self.client = redis.Redis(host=host, port=port, password=password, username=username)
 
-  def add_to_db(self, key: str, value: str):
-    return self.client.set(key, value)
+  def add_to_db(self, name: str, key: str, value: str):
+    return self.client.hset(name, key, value)
 
   def delete_from_db(self, key):
     pass
@@ -14,5 +14,5 @@ class Redis(Database):
   def edit_in_db(self, key: str, value: str):
     pass
 
-  def get_from_db(self, key: str):
-    return self.client.get(key)
+  def get_from_db(self, name: str, key: str):
+    return self.client.hget(name, key)
