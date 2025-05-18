f292a61446ef05aef960dd842dccba02df7b8ed2
Фёдор Ч. <alez1934@yandex.ru>
2025-05-18 10:20:20 +0300

    добавить: метод для возвращения сообщения

--- app/routers/entry.py
+++ app/routers/entry.py
@@ -13,12 +13,19 @@ async def route_entry_post(entry: Entry, response: Response):
       response.status_code = status.HTTP_400_BAD_REQUEST
       recipients = "not a valid PGP message"
     else:
-      get_database().add_to_db(entry.key, entry.message)
+      get_database().add_to_db(f'fingerprint:{entry.fingerprint}', entry.key, entry.message)
     return {"result": recipients}
   else:
     encrypt_result = encrypt_message(entry.message, entry.fingerprint)
     if not encrypt_result.ok:
       response.status_code = status.HTTP_400_BAD_REQUEST
     else:
-      get_database().add_to_db(entry.key, encrypt_result.data)
+      get_database().add_to_db(f'fingerprint:{entry.fingerprint}', entry.key, encrypt_result.data)
     return {"result": encrypt_result.status}
+
+@router.get("/get", status_code=200)
+async def route_entry_get(fingerprint: str, key: str, response: Response):
+  result = get_database().get_from_db(f'fingerprint:{fingerprint}', key)
+  if result == None:
+    response.status_code = status.HTTP_404_NOT_FOUND
+  return {"result": result}
