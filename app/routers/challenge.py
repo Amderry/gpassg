57ae00f7ebb2cef49394c2195aea7e1ce5cd8364
Фёдор Ч. <alez1934@yandex.ru>
2025-05-19 14:04:03 +0300

    измен: небольшой рефакторинг проведён

--- app/routers/challenge.py
+++ app/routers/challenge.py
@@ -10,16 +10,21 @@ router = APIRouter()
 @router.post("/post")
 async def route_challenge_post(challenge: Challenge, response: Response):
   challenge_result = check_challenge(challenge.fingerprint)
-  if not challenge_result:
-    secret = get_database().get_from_db(f'{challenge.fingerprint}:challenge').decode('utf-8')
-    if secret == challenge.secret:
-      get_database().add_to_db(f'{challenge.fingerprint}:challenge_passed', 'true', 120)
+  print(challenge_result, flush=True)
+  secret = get_database().get_from_db(f'{challenge.fingerprint}:challenge')
+
+  if challenge_result == None:
+    message = "no challenge, request one"
+  elif not challenge_result:
+    if secret.decode('utf-8') == challenge.secret:
+      pass_challenge(challenge.fingerprint, 120)
       message = "challenge passed, you have 2 min to go"
     else:
       response.status_code = status.HTTP_401_UNAUTHORIZED
       message = "gtfo"
-  else:
-    message = "no challenge, request one"
+  elif challenge_result:
+    message = "challenge already passed"
+    
   return {"result": message}
 
 @router.get("/get")
@@ -27,14 +32,19 @@ async def route_challenge_get(fingerprint: str, response: Response):
   key = get_database().get_from_db(f'{fingerprint}:publickey')
   if key == None:
     response.status_code = status.HTTP_404_NOT_FOUND
-    message = "key_not_found"
-  import_publickey(key)
-  challenge_result = check_challenge(fingerprint)
-  if not challenge_result:
-    message = "You have 60 secm,eonds before your challenge expires! Your challenge secret:" + create_challenge(fingerprint)
-    message = encrypt_message(message, fingerprint).data
+    message = "key not found"
   else:
-    message = ""
+    import_publickey(key)
+    challenge_result = check_challenge(fingerprint)
+    print(challenge_result, flush=True)
+    if challenge_result == None:
+      message = "You have 60 seconds before your challenge expires! Your challenge secret: " + create_challenge(fingerprint) + '\n'
+      message = encrypt_message(message, fingerprint).data
+    elif not challenge_result:
+      message = "you already have pending challenge"
+    else:
+      message = ""
+
   return {"result": message}
 
 def create_challenge(fingerprint: str):
@@ -46,5 +56,9 @@ def create_challenge(fingerprint: str):
 def check_challenge(fingerprint: str):
   challenge_result = get_database().get_from_db(f'{fingerprint}:challenge_passed')
   if challenge_result != None:
-    challenge_result = challenge_result.decode('utf-8')
-  return challenge_result in ['true']
+    return challenge_result.decode('utf-8') in ['true']
+  else:
+    return None
+
+def pass_challenge(fingerprint: str, ttl: int):
+  get_database().add_to_db(f'{fingerprint}:challenge_passed', 'true', ttl)
