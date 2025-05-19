f37a3ea10da7b042b5adda5bc008375d9bb09987
Фёдор Ч. <alez1934@yandex.ru>
2025-05-19 20:21:45 +0300

    изменить: значительный рефакторинг и удаление дублирующихся методов, перекладывая ответственность на миддлварь

--- app/routers/publickey.py
+++ app/routers/publickey.py
@@ -1,7 +1,8 @@
 from fastapi import status, Body, APIRouter, Depends, Response
 from schemas.publickey import PublicKey
-from utils.gnupg import import_publickey, get_fingerprints, delete_key, encrypt_message 
+from utils.gnupg import import_publickey, get_fingerprints, delete_publickey, encrypt_message 
 from database.database_fabric import get_database
+from .challenge import check_challenge, create_challenge
 import random
 import string
 
@@ -9,33 +10,28 @@ router = APIRouter()
 
 @router.post("/import", status_code=200)
 async def route_publickey_import(publickey: PublicKey, response: Response):
-  database_key = get_database().get_from_db(f'{publickey.fingerprint}:publickey')
-  challenge_pending = get_database().get_from_db(f'{publickey.fingerprint}:import_challenge_passed')
-  if database_key == None and challenge_pending == None:
-    import_result = import_publickey(publickey.key_text)
-    if import_result.returncode != 0:
-      response.status_code = status.HTTP_400_BAD_REQUEST
-      return {"fingerprints": []}
-    secret = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(32))
-    get_database().add_to_db(f'{publickey.fingerprint}:import_challenge', secret, 60)
-    get_database().add_to_db(f'{publickey.fingerprint}:import_challenge_passed', 'false', 60)
-    encrypted_challenge = encrypt_message("You have 60 seconds before your challenge expires! Your challenge secret: " + secret + "\n", publickey.fingerprint).data
-    delete_key(publickey.fingerprint)
-    return {"challenge": encrypted_challenge}
-  elif challenge_pending != None:
-    secret = get_database().get_from_db(f'{publickey.fingerprint}:import_challenge')
-    challenge_passed = get_database().get_from_db(f'{publickey.fingerprint}:import_challenge_passed')
-    if challenge_passed == b'false':
-      if secret.decode('utf-8') ==  publickey.challenge:
-        push_publickey(publickey.key_text, get_database())
-        import_result = import_publickey(publickey.key_text)
-        return {"fingerprints": import_result.fingerprints}
-      else:
-        return {"challenge": "gtfo"}
-  else:
-    return {"fingerprints": f"already imported {publickey.fingerprint}"}
+  push_publickey(publickey.key_text)
+  import_result = import_publickey_wrapper(publickey.key_text)
+  return {"import": import_result}
 
-def push_publickey(publickey: str, database):
-  fingerprints = get_fingerprints(publickey)
+def push_publickey(publickey: str):
+  fingerprints = get_fingerprints_wrapper(publickey)
   for fp in fingerprints:
-    database.add_to_db(f'{fp}:publickey', publickey)
+    get_database().add_to_db(f'{fp}:publickey', publickey)
+
+def del_publickey(fingerprint: str):
+  get_database().delete_from_db(f'{fingerprint}:publickey')
+
+def get_publickey(fingerprint: str):
+  return get_database().get_from_db(f'{fingerprint}:publickey')
+
+def import_publickey_wrapper(key_text: str):
+  push_publickey(key_text)
+  return import_publickey(key_text).fingerprints[0]
+
+def delete_publickey_wrapper(fingerprint: str):
+  del_publickey(fingerprint)
+  return delete_publickey(fingerprint)
+
+def get_fingerprints_wrapper(key_text: str):
+  return get_fingerprints(key_text)
