d65ba1f429086873dab369a7ab9cf922aeff509f
Фёдор Ч. <alez1934@yandex.ru>
2025-05-24 08:05:43 +0300

    добавить: DELETE метод для записей и хэширование сообщения

--- app/routers/entry.py
+++ app/routers/entry.py
@@ -2,30 +2,47 @@ from fastapi import status, Body, APIRouter, Depends, Response
 from schemas.entry import Entry
 from database.database_fabric import get_database
 from utils.gnupg import encrypt_message, get_recipients
+import hashlib
 
 router = APIRouter()
 
 @router.post("/post", status_code=200)
 async def route_entry_post(entry: Entry, response: Response):
+  sha256 = hashlib.sha256()
   if entry.encrypted:
     recipients = get_recipients(entry.message)
     if len(recipients) == 0:
       response.status_code = status.HTTP_400_BAD_REQUEST
       recipients = "not a valid PGP message"
     else:
-      get_database().add_to_db(f'fingerprint:{entry.fingerprint}:message:{entry.key}', entry.message)
+      get_database().add_to_db(f'fingerprint:{entry.fingerprint}:message:{hash_key(entry.key)}', entry.message)
     return {"result": recipients}
   else:
     encrypt_result = encrypt_message(entry.message, entry.fingerprint)
     if not encrypt_result.ok:
       response.status_code = status.HTTP_400_BAD_REQUEST
     else:
-      get_database().add_to_db(f'fingerprint:{entry.fingerprint}:message:{entry.key}', encrypt_result.data)
+      sha256.update(entry.key.encode('utf-8'))
+      hashed_key = sha256.hexdigest()
+      get_database().add_to_db(f'fingerprint:{entry.fingerprint}:message:{hash_key(entry.key)}', encrypt_result.data)
     return {"result": encrypt_result.status}
 
 @router.get("/get", status_code=200)
 async def route_entry_get(fingerprint: str, key: str, response: Response):
-  result = get_database().get_from_db(f'fingerprint:{fingerprint}:message:{key}')
+  result = get_database().get_from_db(f'fingerprint:{fingerprint}:message:{hash_key(key)}')
   if result == None:
     response.status_code = status.HTTP_404_NOT_FOUND
   return {"result": result}
+
+@router.delete("/delete", status_code=200)
+async def route_entry_delete(fingerprint: str, key: str, response: Response):
+  result = get_database().delete_from_db(f'fingerprint:{fingerprint}:message:{hash_key(key)}')
+  if result == None:
+    response.status_code = status.HTTP_404_NOT_FOUND
+  return {"result": result}
+
+def hash_key(key: str):
+  sha256 = hashlib.sha256()
+  key = key.encode('utf-8')
+  sha256.update(key)
+  return sha256.hexdigest()
