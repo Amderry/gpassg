32ef3fc44edb08993769a5336928988d984b79da
Фёдор Ч. <alez1934@yandex.ru>
2025-06-04 08:34:38 +0300

    добавить: своё исключение при отсутствии типа хранилища

--- app/database/database_fabric.py
+++ app/database/database_fabric.py
@@ -2,5 +2,7 @@ from .database_redis import Redis
 import config
 
 def get_database():
-  if config.STORAGE_TYPE.lower() == 'redis':
+  if config.STORAGE_TYPE == None:
+    raise Exception("Storage type is not set")
+  elif config.STORAGE_TYPE.lower() == 'redis':
     return Redis(config.STORAGE_HOST, config.STORAGE_PORT, config.STORAGE_PASSWORD, config.STORAGE_USERNAME)
