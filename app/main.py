411b9f11542562f326ee9eabd2cf33ea95f5603d
Alex1934 <alez1934@yandex.ru>
2025-06-04 11:07:49 +0300

    добавить: объявить ранее описанный класс в main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, Depends
 from routers import publickey, entry, root
 from routers import challenge as challenge_router
-from middleware import challenge
-from starlette.middleware.base import BaseHTTPMiddleware
+from middleware.challenge import ChallengeMiddleware
 
 app = FastAPI()
 app.include_router(root.router)
 app.include_router(publickey.router, prefix="/publickey")
 app.include_router(entry.router, prefix="/entry")
 app.include_router(challenge_router.router, prefix="/challenge")
-app.add_middleware(BaseHTTPMiddleware, dispatch=challenge.challenge_middleware)
+app.add_middleware(ChallengeMiddleware)
