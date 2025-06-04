1913318721cd56519225860569d6581682957d29
Фёдор Ч. <alez1934@yandex.ru>
2025-06-04 08:19:05 +0300

    добавить: корневой путь в main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Depends
-from routers import publickey, entry
+from routers import publickey, entry, root
 from routers import challenge as challenge_router
 from middleware import challenge
 from starlette.middleware.base import BaseHTTPMiddleware
 
 app = FastAPI()
+app.include_router(root.router)
 app.include_router(publickey.router, prefix="/publickey")
 app.include_router(entry.router, prefix="/entry")
 app.include_router(challenge_router.router, prefix="/challenge")
