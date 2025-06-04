a67750e5930431435f8d0545bb10ad357a369084
Alex1934 <alez1934@yandex.ru>
2025-06-04 12:20:20 +0300

    добавить: какую-никакую обработку ошибок в мидлварь

--- app/middleware/challenge.py
+++ app/middleware/challenge.py
@@ -7,23 +7,43 @@ from json import loads
 from urllib.parse import urlencode
 from routers.publickey import import_publickey_wrapper, delete_publickey_wrapper, get_publickey, get_fingerprints_wrapper
 from routers.challenge import check_challenge 
+from pydantic import ValidationError
 
 class ChallengeMiddleware(BaseHTTPMiddleware):
   async def dispatch(self, request, call_next):
     base_path = request.url.path
 
     # Check if incoming request to any of /entry routes have a pending challenge
-    if base_path.startswith('/entry'):
-      fingerprint = extract_fingerprint(request)
+    if base_path in ['/entry/get', '/entry/delete']:
+      try:
+        print(request.query_params['fingerprint'], flush=False)
+        fingerprint = request.query_params['fingerprint']
+      except:
+        return JSONResponse(content={"error": "bad request"}, status_code=status.HTTP_400_BAD_REQUEST)
       challenge_creation_needed = challenge_exists(fingerprint)
       if challenge_creation_needed:
-       return JSONResponse(content={"info": "challenge required"}, status_code=status.HTTP_401_UNAUTHORIZED)
+        return JSONResponse(content={"info": "challenge required"}, status_code=status.HTTP_401_UNAUTHORIZED)
+      return await call_next(request)
+
+    elif base_path in ['/entry/post']:
+      body = await request.body()
+      try:
+        fingerprint = fingerprint = loads(body)['fingerprint']
+      except:
+        return JSONResponse(content={"error": "bad request"}, status_code=status.HTTP_400_BAD_REQUEST)
+      challenge_creation_needed = challenge_exists(fingerprint)
+      if challenge_creation_needed:
+        return JSONResponse(content={"info": "challenge required"}, status_code=status.HTTP_401_UNAUTHORIZED)
       return await call_next(request)
 
     # Proceed incoming request to any of /publickey routes (it requires more complicated logic). Importing publickey and then instantly deleting it assuming that client have no access to his key apriori and if he returns right challenge - import it again, now permanently.
     elif base_path.startswith('/publickey'):
       body = await request.body()
-      fingerprint = get_fingerprints_wrapper(loads(body)['key_text'])[0]
+      print(body, flush=False)
+      try:
+        fingerprint = get_fingerprints_wrapper(loads(body)['key_text'])[0]
+      except:
+        return JSONResponse(status_code=400, content={"error": "bad request"})
       challenge_result = check_challenge(fingerprint)
       publickey_exists = get_publickey(fingerprint)
       if (challenge_result == None or not challenge_result) and not publickey_exists:
@@ -42,10 +62,3 @@ class ChallengeMiddleware(BaseHTTPMiddleware):
 def challenge_exists(fingerprint: str):
   challenge_passed = check_challenge(fingerprint)
   return challenge_passed == None
-
-async def extract_fingerprint(request: Request):
-  if request.method == 'GET' or request.method == 'DELETE':
-    fingerprint = request.query_params['fingerprint']
-  else:
-    fingerprint = loads(await request.body())['fingerprint']
-  return fingerprint
