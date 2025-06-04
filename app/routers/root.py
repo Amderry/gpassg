5bc747dc7af55aa4b325a5f2e3e5003992bdf243
Фёдор Ч. <alez1934@yandex.ru>
2025-06-04 08:20:31 +0300

    изменить: добавить двоеточие при определении функции

--- app/routers/root.py
+++ app/routers/root.py
@@ -4,5 +4,5 @@ from config import VERSION
 router = APIRouter()
 
 @router.get("/", status_code=200)
-async def root_get()
+async def root_get():
   return {"description": "Greetings! This app is a zero-knowledge password vault that uses OpenPGP standart to store and encrypt your messages. In general, it's not limited to store only passwords and sensitive data, every file that can be encrypted lies perfectly in this concept.", "version": VERSION} 
